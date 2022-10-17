4ee104bc2976e9e7563c11ba4f44cc0479169b18
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-17 20:49:58 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+   end
+   def name
+    @this_dogs_name
+  end
 end
+name_dog = Dog.new
+name_dog.name = "rose"
+puts name_dog.name
