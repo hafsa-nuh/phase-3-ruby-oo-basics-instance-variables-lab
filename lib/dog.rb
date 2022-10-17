371bbfaac4428df01102e16368296d13ebb41d94
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-17 21:11:59 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,10 @@
 class Dog
+    # setter method which take an argument and set the instance variable
     def name=(dog_name)
+        # instance variable are bound to an instance
         @this_dogs_name = dog_name
    end
+#    getter method that will return the value of the instance variable
    def name
     @this_dogs_name
   end
@@ -9,3 +12,4 @@ end
 name_dog = Dog.new
 name_dog.name = "rose"
 puts name_dog.name
+
